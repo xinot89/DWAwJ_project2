c47c5a66f495c542fddf66a4e4264756c12d5971
xinot89 <toni.pohjanheimo@gmail.com>
2024-04-24 11:58:38 +0300

    Listing trains to page works again, but sorting by time doesn't.

--- scripts.js
+++ scripts.js
@@ -290,8 +290,6 @@ function loadData(inputdata) {
         }
       }
       if (arrivingBoolean && currentStation == targetStation && type == "ARRIVAL") {
-        console.log("Arrivingboolean triggasi, trigannut ttrow:");
-        console.log(ttrow);
         //Get timedata from JSON to variable:
         timestamp = ttrow.scheduledTime;
         //Make new date object out of it, date object usage also automatically converts time to local time.:
@@ -300,16 +298,12 @@ function loadData(inputdata) {
         //Get hours and minutes from date -object:
         //var hours = date.getHours();
         //var minutes = date.getMinutes();
-        console.log(arrivalTime);
         timetableEntries.push(arrivalTime);
         lastStation = currentStation;
         savedTimes = true;
       } else if (departingBoolean && currentStation == targetStation && type == "DEPARTURE") {
-        console.log("Departingboolean triggasi, trigannut ttrow:");
-        console.log(ttrow);
         timestamp = ttrow.scheduledTime;
         var departureTime = new Date(timestamp);
-        console.log(departureTime);
         timetableEntries.push(departureTime);
         lastStation = currentStation;
         savedTimes = true;
@@ -335,9 +329,9 @@ function loadData(inputdata) {
   //This compares every pair of first entries in subarrays.
   //timetableEntries.sort((a, b) => a[1] - b[1]);
 
-  console.log("This was going to populatetable: "+timetableEntries);
+
   //Finally, call function to put array's data to table:
-  //populatetable(timetableEntries)
+  populatetable(timetableEntries)
 }
 //Function to output array's contents to HTML table.
 function populatetable(dataarray) {
@@ -392,9 +386,17 @@ TableHead.appendChild(TableHeadingRow);
 //Put created table heading -section to table:
 Table.appendChild(TableHead);
 
-//Rolling number for dynamic variables to create new element for each table row and cell:
-tableRowNum = 0;
-tableColumnNum = 0;
+//Rolling number for cells which got their contents via dynamix variables.
+tableComponentNumber = 0;
+//Rolling number for table content rows.
+//Needs to be separate from cells because othervise all cells would be appended to one line.
+tableRowNumber = 0;
+
+//There was some though where this might be necessary, but that thought got lost:
+dateObjectsAfterStopInfo = 0;
+
+//Used for making new row element at start of loop:
+firstloop = true;
 
 
 /*
@@ -407,91 +409,50 @@ departingBoolean
 */
   //Following iterates through every object in data-array and returns train number and other data on same level:
   dataarray.forEach(obj => {
-
-    //Start row processing by generating unique id to row:
-    //Use iteratedTableRow to generate new rows to table:
-    window['iteratedTableRow'+tableRowNum] = document.createElement('tr');
-    //console.log(obj); // This will log each object individually
-    // If you want to access specific properties of each object, you can do so like this:
-    //console.log(obj.timeTableRows); // Replace propertyName with the actual property name you want to access
-    console.log(dataarray);
-    console.log(obj.length);
-
-    if (nonStoppingBoolean) {
-      window['iteratedTableColumn'+tableColumnNum].textContent = obj[0];
-      window['iteratedTableRow'+tableRowNum].appendChild(window['iteratedTableColumn'+tableColumnNum]);
-      //console.log("TableColumnNum ennen plussausta: " + tableColumnNum)
-      tableColumnNum ++;
+    if (firstloop) {
+      window['iteratedTableRow'+tableRowNumber] = document.createElement('tr');
+      firstloop = false;
     }
-    if (arrivedBoolean) {
-      window['iteratedTableColumn'+tableColumnNum].textContent = obj[1];
-      //Needed to add following retrospectively as othervise javascript counts hours+minutes together.
-      window['iteratedTableColumn'+tableColumnNum].textContent += ":"+minutes;
-      window['iteratedTableRow'+tableRowNum].appendChild(window['iteratedTableColumn'+tableColumnNum]);
-      //console.log("TableColumnNum ennen plussausta: " + tableColumnNum)
-      tableColumnNum ++;
+    regexPatternForTrainLetters=/^[A-Z]+$/;
+    regexPatternForTrainStations=/^[A-Z]{2,3}\d{1,}$/;
+    //If input array's first entrry is stopping indicator:
+    if (obj == "Yes." || obj=="No.") {
+      //If non-stopping trains have been requested, put stopping indicator to table:
+      if (nonStoppingBoolean) {
+        //Store current object (stopping indicator) in dynamic variable and append it to table row.
+        window['iterated'+tableComponentNumber] = document.createElement('td');
+        window['iterated'+tableComponentNumber].textContent = obj;
+        window['iteratedTableRow'+tableRowNumber].appendChild(window['iterated'+tableComponentNumber]);
+        tableComponentNumber +=1;
+      }
+      //Found guide to following from: https://stackoverflow.com/questions/2831345/is-there-a-way-to-check-if-a-variable-is-a-date-in-javascript
+    } else if (obj instanceof Date) {
+      /*Take hours and minutes, add leading zero to minutes.*/
+      hours = obj.getHours();
+      minutes = obj.getMinutes();
+      //Add leading zero to minutes if minute -value < 10
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      window['iterated'+tableComponentNumber] = document.createElement('td');
+      window['iterated'+tableComponentNumber].textContent = hours;
+      window['iterated'+tableComponentNumber].textContent += ":"+minutes;
+      window['iteratedTableRow'+tableRowNumber].appendChild(window['iterated'+tableComponentNumber]);
+      dateObjectsAfterStopInfo +=1;
+      tableComponentNumber +=1;
+    } else if (regexPatternForTrainLetters.test(obj)&&obj.length==1) {
+      window['iterated'+tableComponentNumber] = document.createElement('td');
+      window['iterated'+tableComponentNumber].textContent = obj;
+      window['iteratedTableRow'+tableRowNumber].appendChild(window['iterated'+tableComponentNumber]);
+      tableComponentNumber +=1;
+    } else if (obj.length >1 && obj.length <4) {
+      window['iterated'+tableComponentNumber] = document.createElement('td');
+      window['iterated'+tableComponentNumber].textContent = obj;
+      window['iteratedTableRow'+tableRowNumber].appendChild(window['iterated'+tableComponentNumber]);
+      tableComponentNumber +=1;
+    } else if (obj == "NEWTRAIN_6b9d87b08a2ee") {
+      TableBody.appendChild(window['iteratedTableRow'+tableRowNumber]);
+      tableRowNumber += 1;
+      window['iteratedTableRow'+tableRowNumber] = document.createElement('tr');
     }
-    if (arrivingBoolean) {
-      window['iteratedTableColumn'+tableColumnNum].textContent = hours;
-      //Needed to add following retrospectively as othervise javascript counts hours+minutes together.
-      window['iteratedTableColumn'+tableColumnNum].textContent += ":"+minutes;
-      window['iteratedTableRow'+tableRowNum].appendChild(window['iteratedTableColumn'+tableColumnNum]);
-      //console.log("TableColumnNum ennen plussausta: " + tableColumnNum)
-      tableColumnNum ++;
-    }
-    if (departedBoolean) {
-      window['iteratedTableColumn'+tableColumnNum].textContent = hours;
-      //Needed to add following retrospectively as othervise javascript counts hours+minutes together.
-      window['iteratedTableColumn'+tableColumnNum].textContent += ":"+minutes;
-      window['iteratedTableRow'+tableRowNum].appendChild(window['iteratedTableColumn'+tableColumnNum]);
-      //console.log("TableColumnNum ennen plussausta: " + tableColumnNum)
-      tableColumnNum ++;
-    }
-    if (departingBoolean) {
-      window['iteratedTableColumn'+tableColumnNum].textContent = hours;
-      //Needed to add following retrospectively as othervise javascript counts hours+minutes together.
-      window['iteratedTableColumn'+tableColumnNum].textContent += ":"+minutes;
-      window['iteratedTableRow'+tableRowNum].appendChild(window['iteratedTableColumn'+tableColumnNum]);
-      //console.log("TableColumnNum ennen plussausta: " + tableColumnNum)
-      tableColumnNum ++;
-    }
-    //Columns made with rolling number:
-    //"window" packages given string and variable's value as string name, so it suits well this use case.
-    window['iteratedTableColumn'+tableColumnNum] = document.createElement('td');
-    //Parse array entry's time to nice format:
-    var hours = obj[0].getHours();
-    var minutes = obj[0].getMinutes();
-    //Add leading zero to minutes if minute -value < 10
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-
-    window['iteratedTableColumn'+tableColumnNum].textContent = hours;
-    //Needed to add following retrospectively as othervise javascript counts hours+minutes together.
-    window['iteratedTableColumn'+tableColumnNum].textContent += ":"+minutes;
-    window['iteratedTableRow'+tableRowNum].appendChild(window['iteratedTableColumn'+tableColumnNum]);
-    //console.log("TableColumnNum ennen plussausta: " + tableColumnNum)
-    tableColumnNum ++;
-    //console.log("TableColumnNum plussauksen jälkeen: " + tableColumnNum)
-
-    //This is basically new variable, so it's necessary to set this each time separately:
-    window['iteratedTableColumn'+tableColumnNum] = document.createElement('td');
-    window['iteratedTableColumn'+tableColumnNum].textContent = obj[1];
-    window['iteratedTableRow'+tableRowNum].appendChild(window['iteratedTableColumn'+tableColumnNum]);
-    tableColumnNum ++;
-    window['iteratedTableColumn'+tableColumnNum] = document.createElement('td');
-    /*Last row of timetablerows could be good and simple to put into table as destination.
-    There's problem, that commuter trains have only one timetable for day's all trips.
-    Long distance trains timetable seems to end on last stop.
-    For that, i take all timetable's entries into array and take last of them:*/
-    window['iteratedTableColumn'+tableColumnNum].textContent = obj[2];
-    window['iteratedTableRow'+tableRowNum].appendChild(window['iteratedTableColumn'+tableColumnNum]);
-    tableColumnNum ++;
-
-    //And row to Table body:
-    TableBody.appendChild(window['iteratedTableRow'+tableRowNum]);
-    //console.log("Appendin jälkeen: " + window['iteratedTableRow'+tableRowNum].textContent + "Rnro:" + tableRowNum)      
-    
-    //console.log("Appendin jälkeen, ennen lisäystä: " + window['iteratedTableRow'+tableRowNum].textContent + "Rnro:" + tableRowNum)
-    tableRowNum ++;
   });
   //Add table body to table:
 Table.appendChild(TableBody);

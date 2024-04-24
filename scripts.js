b1ef0d7b42a21b482cbbfa6af9f4450da418cb61
xinot89 <toni.pohjanheimo@gmail.com>
2024-04-24 14:59:38 +0300

    Sorting in development.

--- scripts.js
+++ scripts.js
@@ -41,6 +41,18 @@ departedBoolean = false;
 departingBoolean = false;
 nonStoppingBoolean = false;
 
+//Storage for fetched data, so no new fetch is necessary when sorting data:
+//Used by populateTable -function.
+dataarrayStore = []
+
+//Variable to store populatetable's sorting order:
+/*
+0: departures, ascending
+1: departures, descending
+2: arrivals, ascending
+3: arrivals, descending */
+sortorder = 0;
+
 //Event handler function calls encapsulated in anymous function calls, so they aren't called automatically every time that page loads:
 //Listener for Dropdown -menu:
 document.getElementById("stationDropDown").addEventListener('change', function() {
@@ -165,6 +177,11 @@ function initializeLoad(fromwhere) {
   }
   arrivingComponent = "&arriving_trains=0";
   arrivingBoolean = document.getElementById("CheckboxGroup1_1").checked;
+  //Set sorting by selected data if it's set to unselected data:
+  //Sortorder 0-1: departures
+  if (sortorder <-1 && sortorder < 2 && departingBoolean == false) {
+    sortorder = 2;
+  }
   if (arrivingBoolean) {
     arrivingComponent = "&arriving_trains="+fetchcount;
     proceed = true;
@@ -178,6 +195,11 @@ function initializeLoad(fromwhere) {
   }
   departingComponent = "&departing_trains=0";
   departingBoolean = document.getElementById("CheckboxGroup1_3").checked;
+  //Set sorting by selected data if it's set to unselected data:
+  //Sortorder 2-3: arrivals
+  if (sortorder <1 && sortorder < 4 && arrivingBoolean == false) {
+    sortorder = 0;
+  }
   if (departingBoolean) {
     departingComponent = "&departing_trains="+fetchcount;
     proceed = true;
@@ -220,7 +242,7 @@ function datafetch() {
   //When playing with production data, make this function to expect string as input and give that string as parameter to next line instead of sample data.
   
   //Choose sample or production data:
-  fetch('Datasample_short.json')
+  fetch('Datasample.json')
   //fetch(fetchurl);
   
   .then(response => {
@@ -333,31 +355,66 @@ function loadData(inputdata) {
   //This compares every pair of first entries in subarrays.
   //timetableEntries.sort((a, b) => a[1] - b[1]);
 
-
+  //Make sure that populatetable's dataarrayStore is empty, before it ads current data to it:
+  dataarrayStore.length = 0;
   //Finally, call function to put array's data to table:
   populatetable(timetableEntries)
 }
 //Function to output array's contents to HTML table.
 function populatetable(dataarray) {
-  /*Originally i meant to use flat array as input for this function, which i then swapped to subarray -structure
-  and back to flat array when it started feel too complex. Now i again try on this function to split input array to 
-  subarrays for sorting purposes.*/
+  if (dataarray="Sortrequest") {
+    console.log("Sorting request received");
+  } else {
+    //This block was originally start of function, until i started developing sorting.
+    /*Originally i meant to use flat array as input for this function, which i then swapped to subarray -structure
+    and back to flat array when it started feel too complex. Now i again try on this function to split input array to 
+    subarrays for sorting purposes.*/
+    //Split input array to subarrays:
+    const separator = "NEWTRAIN_6b9d87b08a2ee"
+    var arrayOfArrays = [];
+    var startIndex = 0;
 
-  //Split input array to subarrays:
-  const separator = "NEWTRAIN_6b9d87b08a2ee"
-  var arrayOfArrays = [];
-  var startIndex = 0;
-
-  for (var i = 0; i < dataarray.length; i++) {
-      if (dataarray[i] === separator) {
-          // Slice the flat array from startIndex to i and push it to arrayOfArrays
-          arrayOfArrays.push(dataarray.slice(startIndex, i));
-          // Update the startIndex to the next element after the separator
-          startIndex = i + 1;
-      }
+    for (var i = 0; i < dataarray.length; i++) {
+        if (dataarray[i] === separator) {
+            // Slice the flat array from startIndex to i and push it to arrayOfArrays
+            arrayOfArrays.push(dataarray.slice(startIndex, i));
+            //Put processed data to side for potential sorting requests:
+            dataarrayStore.push(dataarray.slice(startIndex, i));
+            // Update the startIndex to the next element after the separator
+            startIndex = i + 1;
+        }
+    }
   }
+//Define where dates are by selections:
+  if (departingBoolean) {
+    departuresPosition = 1;
+    if (arrivingBoolean) {
+      arrivalsPosition = 2;
+    }
+  } else {
+    if (arrivingBoolean) {
+      arrivalsPosition = 1;
+    } else {
+      console.log("Populatetable didn't find selected departures or arrivals.")
+    }
+  }
+//Sort arrays by wanted sorting order:
+//0: departures, ascending:
+if (sortorder = 0) {
+arrayOfArrays.sort((a, b) => a[departuresPosition] - b[departuresPosition]);
+//1: departures, descending:
+} else if (sortorder = 1) {
+  arrayOfArrays.sort((b, a) => a[departuresPosition] - b[departuresPosition]);
+//2: arrivals, ascending:
+} else if (sortorder = 2) {
+  arrayOfArrays.sort((a, b) => a[arrivalsPosition] - b[arrivalsPosition]);
+//3: arrivals, descending:
+} else if (sortorder = 3) {
+ arrayOfArrays.sort((b, a) => a[arrivalsPosition] - b[arrivalsPosition]);
+} else {
+  console.log("Populatetable didn't get correct sort order parameter.")
+}
 
-  arrayOfArrays.sort((a, b) => a[1] - b[1]);
   console.log(arrayOfArrays)
 
   //Define different table's components:
@@ -369,9 +426,9 @@ function populatetable(dataarray) {
   const TableHeadingRow = document.createElement('tr');
   //Table's heading cells:
   const doesTrainStop = document.createElement('th');
-  const ArrivedTime = document.createElement('th');
+  //const ArrivedTime = document.createElement('th');
   const ArrivingTime = document.createElement('th');
-  const DepartedTime = document.createElement('th');
+  //const DepartedTime = document.createElement('th');
   const DepartureTime = document.createElement('th');
 
   const TrainLetter = document.createElement("th");
@@ -382,19 +439,19 @@ if (nonStoppingBoolean) {
   doesTrainStop.textContent="Stopping?";
   TableHeadingRow.appendChild(doesTrainStop);
 }
-if (arrivedBoolean) {
+/*if (arrivedBoolean) {
   ArrivedTime.textContent ="Arrived";
   //Append heading column:
   TableHeadingRow.appendChild(ArrivedTime);
-}
+}*/
 if (arrivingBoolean) {
   ArrivingTime.textContent = "Arriving";
   TableHeadingRow.appendChild(ArrivingTime);
 }
-if (departedBoolean) {
+/*if (departedBoolean) {
   DepartedTime.textContent = "Departed";
   TableHeadingRow.appendChild(DepartedTime);
-}
+}*/
 if (departingBoolean) {
   DepartureTime.textContent ="Departure";
   TableHeadingRow.appendChild(DepartureTime);
@@ -421,16 +478,55 @@ dateObjectsAfterStopInfo = 0;
 //Used for making new row element at start of loop:
 firstloop = true;
 
-
-/*
-Boolean values used: 
-nonStoppingBoolean
-arrivedBoolean
-arrivingBoolean
-departedBoolean
-departingBoolean
-*/
-
+    //Following iterates through every object in data-array and returns train number and other data on same level:
+    dataarray.forEach(obj => {
+      if (firstloop) {
+        window['iteratedTableRow'+tableRowNumber] = document.createElement('tr');
+        firstloop = false;
+      }
+      regexPatternForTrainLetters=/^[A-Z]+$/;
+      regexPatternForTrainStations=/^[A-Z]{2,3}\d{1,}$/;
+      //If input array's first entrry is stopping indicator:
+      if (obj == "Yes." || obj=="No.") {
+        //If non-stopping trains have been requested, put stopping indicator to table:
+        if (nonStoppingBoolean) {
+          //Store current object (stopping indicator) in dynamic variable and append it to table row.
+          window['iterated'+tableComponentNumber] = document.createElement('td');
+          window['iterated'+tableComponentNumber].textContent = obj;
+          window['iteratedTableRow'+tableRowNumber].appendChild(window['iterated'+tableComponentNumber]);
+          tableComponentNumber +=1;
+        }
+        //Found guide to following from: https://stackoverflow.com/questions/2831345/is-there-a-way-to-check-if-a-variable-is-a-date-in-javascript
+      } else if (obj instanceof Date) {
+        /*Take hours and minutes, add leading zero to minutes.*/
+        hours = obj.getHours();
+        minutes = obj.getMinutes();
+        //Add leading zero to minutes if minute -value < 10
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        window['iterated'+tableComponentNumber] = document.createElement('td');
+        window['iterated'+tableComponentNumber].textContent = hours;
+        window['iterated'+tableComponentNumber].textContent += ":"+minutes;
+        window['iteratedTableRow'+tableRowNumber].appendChild(window['iterated'+tableComponentNumber]);
+        dateObjectsAfterStopInfo +=1;
+        tableComponentNumber +=1;
+      } else if (regexPatternForTrainLetters.test(obj)&&obj.length==1) {
+        window['iterated'+tableComponentNumber] = document.createElement('td');
+        window['iterated'+tableComponentNumber].textContent = obj;
+        window['iteratedTableRow'+tableRowNumber].appendChild(window['iterated'+tableComponentNumber]);
+        tableComponentNumber +=1;
+      } else if (obj.length >1 && obj.length <4) {
+        window['iterated'+tableComponentNumber] = document.createElement('td');
+        window['iterated'+tableComponentNumber].textContent = obj;
+        window['iteratedTableRow'+tableRowNumber].appendChild(window['iterated'+tableComponentNumber]);
+        tableComponentNumber +=1;
+      } else if (obj == "NEWTRAIN_6b9d87b08a2ee") {
+        TableBody.appendChild(window['iteratedTableRow'+tableRowNumber]);
+        tableRowNumber += 1;
+        window['iteratedTableRow'+tableRowNumber] = document.createElement('tr');
+      }
+    });
+    //Add table body to table:
+  Table.appendChild(TableBody);
 
 //Clear target div before appending table:
 targetdiv.innerHTML = "";
